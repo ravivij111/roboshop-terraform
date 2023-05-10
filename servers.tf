9d132e6a0fa92676f3832d703c4f9930e3c4cea5
Your Name <you@example.com>
2023-05-10 17:29:37 +0800

    Terraform

--- servers.tf
+++ servers.tf
@@ -9,7 +9,7 @@ output "ami" {
 }
 
 resource "aws_instance" "R1_frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -17,9 +17,16 @@ resource "aws_instance" "R1_frontend" {
   }
 }
 
+resource "aws_route53_record" "R1_frontend" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "frontend-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_frontend.private_ip]
+}
 
-resource "aws_instance" "R1_mongod" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+resource "aws_instance" "R1_mongodb" {
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -27,76 +34,141 @@ resource "aws_instance" "R1_mongod" {
   }
 }
 
+resource "aws_route53_record" "R1_mongodb" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "mongodb-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_mongodb.private_ip]
+}
+
+
 resource "aws_instance" "R1_Redis" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_Redis"
   }
 }
+resource "aws_route53_record" "R1_Redis" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "redis-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_Redis.private_ip]
+}
 
 resource "aws_instance" "R1_catalogue" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_catalogue"
   }
 }
+resource "aws_route53_record" "R1_catalogue" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "catalogue-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_catalogue.private_ip]
+}
 
 resource "aws_instance" "R1_cart" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_cart"
   }
 }
+resource "aws_route53_record" "R1_cart" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "cart-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_cart.private_ip]
+}
 
 resource "aws_instance" "R1_user" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_user"
   }
 }
+resource "aws_route53_record" "R1_user" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "user-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_user.private_ip]
+}
 
 resource "aws_instance" "R1_mysql" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_mysql"
   }
 }
+resource "aws_route53_record" "R1_mysql" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "mysql-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_mysql.private_ip]
+}
 
 resource "aws_instance" "R1_shipping" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_shipping"
   }
 }
+resource "aws_route53_record" "R1_shipping" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "shipping-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_shipping.private_ip]
+}
 
 resource "aws_instance" "R1_rabbitmq" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_rabbitmq"
   }
 }
+resource "aws_route53_record" "R1_rabbitmq" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "rabbitmq-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_rabbitmq.private_ip]
+}
 
 resource "aws_instance" "R1_payment" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "R1_payment"
   }
+}
+resource "aws_route53_record" "R1_payment" {
+  zone_id = "Z09746683LPCR02M9AALO"
+  name    = "payment-dev.r1devopsb.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.R1_payment.private_ip]
 }
 
 output "R1_Payment" {
