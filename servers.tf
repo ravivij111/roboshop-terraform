3e91c61b532b76dbd81d80f0dedd6ca9b8fb395a
Your Name <you@example.com>
2023-05-10 16:45:12 +0800

    Terraform

---
+++ servers.tf
@@ -0,0 +1,90 @@
+resource "aws_instance" "R1_frontend" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_frontend"
+  }
+}
+
+
+resource "aws_instance" "R1_mongod" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_mongodb"
+  }
+}
+
+resource "aws_instance" "R1_Redis" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_Redis"
+  }
+}
+
+resource "aws_instance" "R1_catalogue" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_catalogue"
+  }
+}
+
+resource "aws_instance" "R1_cart" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_cart"
+  }
+}
+
+resource "aws_instance" "R1_user" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_user"
+  }
+}
+
+resource "aws_instance" "R1_mysql" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_mysql"
+  }
+}
+
+resource "aws_instance" "R1_shipping" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_shipping"
+  }
+}
+
+resource "aws_instance" "R1_rabbitmq" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_rabbitmq"
+  }
+}
+
+resource "aws_instance" "R1_payment" {
+  ami           = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "R1_payment"
+  }
+}
