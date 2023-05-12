5007eddfe62eb431a1486dd77c6c671742f88f9f
Your Name <you@example.com>
2023-05-13 07:50:56 +0800

    Terraform

--- state.tf
+++ state.tf
@@ -7,12 +7,12 @@ teraform {
   }
 
 }*/
-
-teraform {
+/*
+terraform {
   backend "s3" {
     bucket = "terraform-a72"
     key    = "roboshop/dev/terraform.tfstate"
     region = "us-east-1"
   }
 
-}
+}*/
