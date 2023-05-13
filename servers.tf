78f9839d44141fcac7539e80bd99336b36add7eb
Your Name <you@example.com>
2023-05-13 08:01:34 +0800

    Terraform

--- servers.tf
+++ servers.tf
@@ -1,56 +1,6 @@
+
 variable "variance_type" {
   default = "t3.small"
-}
-variable "components" {
-  default = {
-
-    frontend = {
-      name ="frontend"
-      instance_type = "t3.small"
-    }
-
-    mongodb = {
-      name ="mongodb"
-    instance_type = "t3.micro"
-    }
-    catalogue = {
-      name ="catalogue"
-      instance_type = "t3.micro"
-    }
-    redis = {
-      name ="redis"
-    instance_type = "t3.small"
-    }
-    user = {
-      name ="user"
-      instance_type = "t3.small"
-    }
-    cart = {
-      name ="cart"
-    instance_type = "t3.small"
-    }
-    mysql = {
-      name ="mysql"
-      instance_type = "t3.small"
-    }
-    shipping = {
-      name ="shipping"
-    instance_type = "t3.medium"
-    }
-    rabbitmq= {
-      name ="rabbitmq"
-      instance_type = "t3.small"
-    }
-    payment = {
-     name ="payment"
-    instance_type = "t3.small"
-    }
-    dispatch = {
-      name ="dispatch"
-      instance_type = "t3.small"
-    }
-  }
-
 }
 data "aws_security_group" "Ravi_Secuity_All" {
   name = "Ravi_Secuity_All"
