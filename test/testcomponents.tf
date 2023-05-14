04d5deb981711aea03b192d4cb0206780377d67c
Your Name <you@example.com>
2023-05-14 22:53:37 +0800

    Terraform

---
+++ test/testcomponents.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "instance" {
+  for_each               = var.components
+
+  provisioner "local-exec" {
+
+    command = "echo **** Key Name - ${each.value["instance_type"]} *"
+
+  }
+}
