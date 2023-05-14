3c3febaae2a8616dfaf2346858df6f150d0da8f2
Your Name <you@example.com>
2023-05-14 16:31:57 +0800

    Terraform

--- servers.tf
+++ servers.tf
@@ -1,13 +1,14 @@
 
 resource "aws_instance" "instance" {
-  for_each = var.components
-  ami = data.aws_ami.centos.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids  = [ data.aws_security_group.Ravi_Secuity_All.id ]
-  tags = {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.Ravi_Secuity_All.id]
+  tags                   = {
     Name = each.value["name"]
   }
-
+}
+/*
   provisioner "remote-exec" {
       connection {
         type     = "ssh"
@@ -36,7 +37,7 @@ resource "aws_route53_record" "records" {
   #records = [ aws_instance.instance[each.value["name"]].private_ip ]
 }
 
-
+*/
 
 
 /*
