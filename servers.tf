cdee99e615b2682c2b0a9ff073326b984dacd90c
Your Name <you@example.com>
2023-05-22 18:43:36 +0800

    Terraform

--- servers.tf
+++ servers.tf
@@ -49,6 +49,22 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+     " rm -rf roboshop-shell ",
+      "git clone https://github.com/ravivij111/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}"
+    ]
+  }
 }
 
 resource "aws_route53_record" "records" {
